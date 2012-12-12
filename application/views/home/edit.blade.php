9fd21a5f70e413d34c6fe787070238d566b86cb3
Abby <abigail.chao@gmail.com>
2012-12-12 00:21:23 -0800

    added labels

--- application/views/home/edit.blade.php
+++ application/views/home/edit.blade.php
@@ -14,42 +14,42 @@
 	{{ $errors->has('linkedin') ? 'Please enter a valid LinkedIn URL.<br>' : '' }}
 	<form action="<?php echo URL::to('home/edited') ?>" method="post">
 		<input type='hidden' name='bro_id' value='<?php echo $bro->bro_id ?>'>
-		First Name: <input type='text' name='fname' value='<?php echo $bro->bro_fname ?>' /><br>
-		Last Name: <input type='text' name='lname' value='<?php echo $bro->bro_lname ?>' /><br>
-		Status: <select name='status_id'>
+		<label>First Name:</label><input type='text' name='fname' value='<?php echo $bro->bro_fname ?>' /><br>
+		<label>Last Name:</label><input type='text' name='lname' value='<?php echo $bro->bro_lname ?>' /><br>
+		<label>Status:</label><select name='status_id'>
 			<option name='status_id' value='<?php echo $bro->status_id ?>'><?php echo $bro->status_name ?></option>
 			<?php foreach ($options['statuses'] as $status) : ?>
 				<option name='status_id' value='<?php echo $status->status_id ?>'><?php echo $status->status_name ?></option>
 			<?php endforeach ?>
 		</select><br>
-		Pledge Class: <select name='pc_sem_id'>
+		<label>Pledge Class:</label><select name='pc_sem_id'>
 			<option name='pc_sem_id' value='<?php echo $bro->pc_sem_id ?>'><?php echo $bro->pc_sem ?></option>
 			<?php foreach ($options['semesters'] as $semester) : ?>
 				<option name='pc_sem_id' value='<?php echo $semester->semester_id ?>'><?php echo $semester->semester_name ?></option>
 			<?php endforeach ?>
 		</select>
 		<input type='text' name='pc_year' value='<?php echo $bro->pc_year ?>'><br>
-		Graduation: <select name='grad_sem_id'>
+		<label>Graduation:</label><select name='grad_sem_id'>
 			<option name='grad_sem_id' value='<?php echo $bro->grad_sem_id ?>'><?php echo $bro->grad_sem ?></option>
 				<?php foreach ($options['semesters'] as $semester) : ?>
 				<option name='grad_sem_id' value='<?php echo $semester->semester_id ?>'><?php echo $semester->semester_name ?></option>
 			<?php endforeach ?>
 		</select>
 		<input type='text' name='grad_year' value='<?php echo $bro->grad_year ?>' ><br>
-		Family: <select name='family_id'>
+		<label>Family:</label><select name='family_id'>
 			<option name='family_id' value='<?php echo $bro->family_id ?>'><?php echo $bro->family_name ?></option>
 			<?php foreach ($options['families'] as $family) : ?>
 				<option name='family_id' value='<?php echo $family->family_id ?>'><?php echo $family->family_name ?></option>
 			<?php endforeach ?>
 		</select><br>
-		Big Bro: <select name='bigbro_id' id='combobox'>
+		<label>Big Bro:</label><select name='bigbro_id' id='combobox'>
 			<option name='bigbro_id' value='<?php echo $bro->bigbro_id ?>'><?php echo Roster::getNameById($bro->bigbro_id) ?></option>
 			<?php foreach ($options['roster'] as $bigbro) : ?>
 				<option name='bigbro_id' value='<?php echo $bigbro->bro_id ?>'><?php echo $bigbro->bro_fname.' '.$bigbro->bro_lname ?></option>
 			<?php endforeach ?>
 		</select><br>
-		Email: <input type='text' name='email' value='<?php echo $bro->email ?>' /><br>
-		LinkedIn: <input type='text' name='linkedin' value='<?php echo $bro->linkedin ?>' /><br>
+		<label>Email:</label><input type='text' name='email' value='<?php echo $bro->email ?>' /><br>
+		<label>LinkedIn:</label><input type='text' name='linkedin' value='<?php echo $bro->linkedin ?>' /><br>
 		<input type="submit" value="Submit Edits" />
 	</form>
 	@include('home.footer')
