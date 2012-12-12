6957af600c047682f0609daabfb4692f67fad727
Abby <abigail.chao@gmail.com>
2012-12-11 17:51:37 -0800

    integrated user table

--- application/models/admin.php
+++ application/models/admin.php
@@ -1,8 +1,6 @@
 <?php
 
 class Admin {
-	private static $password = 'may1922';
-
 	public static function verifyPass($input) {
 		if($input == getPassword()) {
 			$_SESSION['admin'] = true;
@@ -13,7 +11,7 @@ class Admin {
 		return $_SESSION['admin'];
 	}
 
-	public static function getPassword() {
+	private static function getPassword() {
 		$results = DB::table('user')->where('username','=','admin')->get(array('password'));
 		$password = $results[0]->password;
 		return $password;
